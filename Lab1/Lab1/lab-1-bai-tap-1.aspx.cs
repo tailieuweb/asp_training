e4a25cbcb276f8487e681c984108a88aa71052c8
Ho Ngu Dat <hongudat@gmail.com>
2023-12-05 01:25:33 +0700

    Done Lab-1

---
+++ Lab1/Lab1/lab-1-bai-tap-1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lab1
+{
+    public partial class lab_1_bai_tap_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Menu1_MenuItemClick(object sender, MenuEventArgs e)
+        {
+            string targetPage = e.Item.Value;
+            Response.Redirect(targetPage);
+        }
+        protected void button_tong_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(input_a.Text);
+            int b = int.Parse(input_b.Text);
+            int tong = a + b;
+            resultLabel.Text = $"Tổng 2 số là {tong}";
+        }
+    }
+}
